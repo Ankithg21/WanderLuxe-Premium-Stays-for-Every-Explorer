2366bf579d37f29899fa7f055ca317de0795ef30
Ankithg21 <ankithg2104@gmail.com>
2025-02-09 20:04:30 +0530

    success and error flashes implemented, and for autnetication is implemented

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ const ExpressError=require("./utils/ExpressError.js");
 const Listing=require("./models/listing.js");
 const {listingSchema,reviewSchema}=require("./schema.js");
 const Review=require("./models/review.js");
+const session=require("express-session");
+const flash=require("connect-flash");
+const passport=require("passport");
+const LocalStrategy=require("passport-local");
+const User=require("./models/user.js");
 
 const listings=require("./routes/listings.js");
 const reviews=require("./routes/review.js");
@@ -31,6 +36,33 @@ main().then((res)=>{
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/DataBaseAIRBNB');
 }
+
+const sessionOption={
+    secret:"mysupersecretcode",
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+        expires:Date.now() + 7*24*60*60*1000,
+        maxAge:7*24*60*60*1000,
+        httpOnly:true
+    }
+};
+
+app. use(session(sessionOption));
+app.use(flash());
+
+// Authentication MiddleWares.
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(LocalStrategy(User.authenticate()));
+passport.serializeUser(User.deserializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req,res,next)=>{
+    res.locals.success=req.flash("success");
+    res.locals.error=req.flash("error");
+    next();
+});
 
 app.use("/listings",listings);
 app.use("/listings/:id/reviews",reviews);
--- routes/listings.js
+++ routes/listings.js
@@ -32,6 +32,7 @@ router.post("/",validateListing,wrapAsync(async(req,res)=>{
     let listing=req.body.listing;
     let newListing=new Listing(listing);
     await newListing.save();
+    req.flash("success","New Listing Created!");
     res.redirect("/listings");
 }));
 
@@ -39,6 +40,10 @@ router.post("/",validateListing,wrapAsync(async(req,res)=>{
 router.get("/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     const listing=await Listing.findById(id).populate("reviews");
+    if(!listing){
+        req.flash("error","Listing you requested for does not exist");
+        res.redirect("/listings");
+    }
     res.render("listings/show.ejs",{listing});
 }));
 
@@ -46,6 +51,10 @@ router.get("/:id",wrapAsync(async(req,res)=>{
 router.get("/:id/edit",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     const listing=await Listing.findById(id);
+    if(!listing){
+        req.flash("error","Listing you requested for does not exist");
+        res.redirect("/listings");
+    }
     res.render("listings/edit.ejs",{listing});
 }));
 
@@ -56,6 +65,7 @@ router.put("/:id",wrapAsync(async(req,res)=>{
     }
     let {id}=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    req.flash("success","Listing Updated!");
     res.redirect("/listings");
 }));
 
@@ -63,6 +73,7 @@ router.put("/:id",wrapAsync(async(req,res)=>{
 router.delete("/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     await Listing.findByIdAndDelete(id);
+    req.flash("success","Listing Deleted!");
     res.redirect("/listings");
 }));
 
